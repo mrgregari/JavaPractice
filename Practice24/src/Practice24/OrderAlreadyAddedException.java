e27ad16b10f8654644b382dea8ebdc2255172526
mrgregari <72759004+mrgregari@users.noreply.github.com>
2023-11-25 11:37:05 +0300

    Add files via upload

---
+++ Practice24/src/Practice24/OrderAlreadyAddedException.java
@@ -0,0 +1,7 @@
+package Practice24;
+
+public class OrderAlreadyAddedException extends RuntimeException {
+    public OrderAlreadyAddedException(String errormessage) {
+        super(errormessage);
+    }
+}
